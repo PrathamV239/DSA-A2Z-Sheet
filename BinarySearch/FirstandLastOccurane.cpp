636ea3e100dbc6985d0122b67b8c7dc9228ac5ca
PrathamV239 <prathamvidhani239@gmail.com>
2026-01-22 08:01:45 +0530

    SOrting

--- BinarySearch/FirstandLastOccurane.cpp
+++ BinarySearch/FirstandLastOccurane.cpp
@@ -2,13 +2,71 @@
 using namespace std;
 
 
-//Brute force approach 
+//Brute force approach: First Occurance iterate from the start. Last Occurance:  Iterate the array in reverse.
 
+//optimised
+pair<int,int> FirstAndLast(vector<int> &nums, int target){
 
+  int n = nums.size();
+
+//First Occurance 
+int low = 0;
+int high = n-1;
+int first = -1;
+
+while (low <= high){
+
+int mid = low +(high - low) /2;
+
+if (nums[mid] == target){
+first = mid ;
+high = mid -1;
+
+}
+else if(nums[mid] < target){
+  low = mid + 1;
+
+}
+else {
+ high = mid -1;
+}
+}
+// Last Occurance 
+
+low =0;
+high = n-1;
+int last = -1;
+
+while (low <= high){
+
+  int mid = low +(high - low) /2;
+
+  if (nums[mid] == target){
+    last = mid ;
+    low = mid+1;
+  }  
+
+  else if (nums[mid]<= target){
+    low = mid +1;
+
+  }
+
+  else {
+    high = mid -1;
+  }
+
+}
+return {first,last};
+}
 
 
 int main(){
+  vector <int> nums = {1,2,3,3,3,4,4,5,5,5,6};
+  int target = 3;
+  pair <int,int>result = FirstAndLast(nums,target);
 
+  cout << "First Occurance is: "<<result.first<<endl;
+  cout <<"Last Occurance is: "<< result.second<<endl;
 
-
+return 0;
 }
