a9a53b7e090f68410ded0d0bd7dcc6c46bbcccb2
PrathamV239 <prathamvidhani239@gmail.com>
2026-01-23 14:46:51 +0530

    sorting

---
+++ Sorting/SelectionSort.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void SelectionSort(vector <int>&nums, int n){
+
+  for(int i=0; i< n-1; i++){
+
+    int mini = i;
+    for (int j = i+1; j<n; j++){
+      if (nums[mini] > nums[j]){
+        mini = j ;
+      }
+    }
+      int temp = nums [mini];
+      nums[mini] = nums [i];
+      nums[i] = temp ;
+    
+
+  }
+  cout << "After selection sort: \n";
+  for(int i =0; i<n;i++){
+    cout << nums[i] << " ";
+  }
+  cout << "\n";
+
+
+}
+
+
+int main(){
+
+ // Initialize array
+    vector <int> arr = {13, 46, 24, 52, 20, 9};
+    int n = arr.size();
+
+    // Print array before sorting
+    cout << "Before selection sort: " << "\n";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+
+
+    SelectionSort(arr, n);
+
+    return 0;
+
+}
